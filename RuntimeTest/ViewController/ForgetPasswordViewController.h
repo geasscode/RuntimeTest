6c21af063e7fd04336b23c4bb6bded3c9f06f2c8
desmond.dong <desmond.dong@easternphoenix.com>
2016-07-11 15:15:30 +0800

    UI Test

---
+++ RuntimeTest/ViewController/ForgetPasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  ForgetPasswordViewController.h
+//  RuntimeTest
+//
+//  Created by tyrael on 16/7/11.
+//  Copyright © 2016年 geasscode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ForgetPasswordViewController : UIViewController
+
+@end
---
+++ RuntimeTest/ViewController/UpdatePasswordViewController.h
@@ -0,0 +1,13 @@
+//
+//  UpdatePasswordViewController.h
+//  RuntimeTest
+//
+//  Created by tyrael on 16/7/11.
+//  Copyright © 2016年 geasscode. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UpdatePasswordViewController : UIViewController
+
+@end
